1eb5d9f98ab1f2856008f97679ada59b26600d84
Patryk Zielinski <patryk.zielinski@outlook.com>
2023-08-22 07:30:27 +0200

    Add async support in repository

--- src/Infrastructure/Repositories/InMemoryHabitRepository.cs
+++ src/Infrastructure/Repositories/InMemoryHabitRepository.cs
@@ -5,26 +5,46 @@ namespace Infrastructure.Repositories;
 
 public class InMemoryHabitRepository : IHabitRepository
 {
-    private static readonly ISet<Habit> _habit = new HashSet<Habit>();
+    private static readonly ISet<Habit> _habit = new HashSet<Habit>()
+    {
+        new Habit(){Name = "Learn .NET",Description = "Nothig",IsCompleted = true},
+        new Habit(){Name = "Learn English",Description = "Nothig",IsCompleted = true,},
+    };
+    
+    
 
     public async Task<List<Habit>> GetAllAsync()
     {
-        return _habit.ToList();
+        var tasks = await Task.FromResult(_habit.ToList());
+        return tasks;
     }
 
     public async Task<Habit> GetByIdAsync(int id)
     {
-        return _habit.SingleOrDefault(x => x.Id == id);
+        var habit = await Task.FromResult(_habit.SingleOrDefault(x => x.Id == id));
+        return habit;
     }
 
     public async Task AddAsync(Habit habit)
     {
-        _habit.Add(habit);
+        await Task.Run(() => _habit.Add(habit));
     }
 
     public async Task UpdateAsync(Habit habit)
     {
-        throw new NotImplementedException();
+        await Task.Run(() => 
+        {
+            var existingHabit = _habit.FirstOrDefault(h => h.Id == habit.Id);
+            if (existingHabit != null)
+            {
+                existingHabit.Name = habit.Name;
+                existingHabit.Description = habit.Description;
+            }
+            else
+            {
+                throw new ArgumentException("Habit not found");
+            }
+        });
     }
 
     public Task DeleteAsync(Habit habit)
