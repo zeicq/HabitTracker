c3f2cb422b143afd3ed52acdd60fab64f56f3e7d
zeicq <patryk-zielinski@outlook.com>
2023-08-28 23:37:17 +0200

    Add properties in the Schedule.cs entity

--- src/Domain/Entity/Schedule.cs
+++ src/Domain/Entity/Schedule.cs
@@ -7,7 +7,12 @@ public class Schedule : BaseEntity
 {
     public ICollection<DaysOfWeekEnum> DaysOfWeek { get; set; }
     public TimeSpan TimeOfDay { get; set; }
+    //TODO
+    public DateTime StartHabit { get; set; }
+    //TODO
+    public DateTime EndHabit { get; set; }
+    //TODO
+    public IEnumerable<DateTime> Reminders { get; set; } 
     public int HabitId { get; set; }
-
     public Habit Habit { get; set; }
 }
---
+++ src/Infrastructure/Migrations/20230828210411_AddTablesForHabit.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTablesForHabit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedBy",
+                table: "Habits",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Habits",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "Habits",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Progress",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HabitId = table.Column<int>(type: "int", nullable: false),
+                    CurrentStreakCount = table.Column<int>(type: "int", nullable: false),
+                    LongestStreakCount = table.Column<int>(type: "int", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Progress", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Progress_Habits_HabitId",
+                        column: x => x.HabitId,
+                        principalTable: "Habits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HabitId = table.Column<int>(type: "int", nullable: false),
+                    DaysOfWeek = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TimeOfDay = table.Column<TimeSpan>(type: "time", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Schedules_Habits_HabitId",
+                        column: x => x.HabitId,
+                        principalTable: "Habits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Progress_HabitId",
+                table: "Progress",
+                column: "HabitId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_HabitId",
+                table: "Schedules",
+                column: "HabitId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Progress");
+
+            migrationBuilder.DropTable(
+                name: "Schedules");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedBy",
+                table: "Habits",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Habits",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "Habits",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
