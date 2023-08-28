3f298cf17aadc0d63d2c75aaa2d7525916caf5d1
zeicq <patryk-zielinski@outlook.com>
2023-08-28 23:17:21 +0200

    change the order in entities

--- src/Domain/Entity/Schedule.cs
+++ src/Domain/Entity/Schedule.cs
@@ -5,9 +5,9 @@ namespace Domain.Entity;
 
 public class Schedule : BaseEntity
 {
-    public int HabitId { get; set; }
     public ICollection<DaysOfWeekEnum> DaysOfWeek { get; set; }
     public TimeSpan TimeOfDay { get; set; }
+    public int HabitId { get; set; }
 
     public Habit Habit { get; set; }
 }
