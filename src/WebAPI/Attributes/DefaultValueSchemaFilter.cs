c9a6b0d78dfe15c8885ed0c15b98f3af51c0d07b
zeicq <patryk-zielinski@outlook.com>
2023-08-29 23:31:39 +0200

    Add SchemaFiler for Swagger

---
+++ src/WebAPI/Attributes/DefaultValueSchemaFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace WebAPI.Attributes;
+
+public class DefaultValueSchemaFilter : ISchemaFilter
+{
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        foreach (var prop in context.Type.GetProperties())
+        {
+            var defaultValueAttr = prop.GetCustomAttribute<DefaultValueAttribute>();
+            if (defaultValueAttr != null && schema.Properties.ContainsKey(prop.Name))
+            {
+                schema.Properties[prop.Name].Default = new OpenApiString(defaultValueAttr.Value.ToString());
+            }
+        }
+    }
+}
--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,14 +1,15 @@
 using Application;
 using Infrastructure;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using WebAPI.Attributes;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(c => { c.SchemaFilter<DefaultValueSchemaFilter>(); });
 builder.Services.ApplicationServices();
 var configuration = builder.Configuration;
 builder.Services.InfrastructureServices(configuration);
