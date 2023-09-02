831f4a99ab664aac9a3855586ce69aa1ca7819b8
zeicq <patryk-zielinski@outlook.com>
2023-09-02 21:50:58 +0200

    Implementation of INotification between Schedule and ScheduleEntry

---
+++ src/Application/Features/ScheduleEntries/Add/CreateScheduleEntryNotificationHandler.cs
@@ -0,0 +1,50 @@
+﻿using Application.Features.Habits.Queries;
+using Application.Features.Schedules.Notifiaction;
+using Application.Shared;
+using AutoMapper;
+using Domain.Entity;
+using Domain.Enums;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Features.ScheduleEntries.Add;
+
+public class CreateScheduleEntryNotificationHandler : INotificationHandler<ScheduleCreatedNotification>
+{
+    private readonly IScheduleEntryRepository _repository;
+
+    public CreateScheduleEntryNotificationHandler(IScheduleEntryRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(ScheduleCreatedNotification request, CancellationToken cancellationToken)
+    {
+        var daysEnum = request.ScheduleViewModel.DaysOfWeek;
+        var startDate = request.ScheduleViewModel.StartData;
+        var timeOfDay = request.ScheduleViewModel.TimeOfDay;
+        var scheduleId = request.ScheduleViewModel.Id;
+
+        int maxEntries = 66;
+        int dayCount = 1;
+
+        while (maxEntries > 0)
+        {
+            if (daysEnum.Contains((DaysOfWeekEnum)startDate.DayOfWeek))
+            {
+                await _repository.AddAsync(new ScheduleEntry
+                {
+                    Date = startDate + timeOfDay,
+                    Day = dayCount,
+                    IsCompleted = false,
+                    ScheduleId = scheduleId
+                });
+
+                maxEntries--;
+                dayCount++;
+            }
+
+            startDate = startDate.AddDays(1);
+        }
+    }
+}
---
+++ src/Application/Features/Schedules/Notifiaction/ScheduleCreatedNotification.cs
@@ -0,0 +1,11 @@
+﻿using Application.Features.Schedules.Queries;
+using Application.Shared;
+using Domain.Entity;
+using MediatR;
+
+namespace Application.Features.Schedules.Notifiaction;
+
+public class ScheduleCreatedNotification:INotification
+{
+    public ScheduleViewModel ScheduleViewModel { get; set; }
+}
