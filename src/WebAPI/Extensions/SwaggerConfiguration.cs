0b367cc9120afad97021b80aa44dba0208f44890
zeicq <patryk-zielinski@outlook.com>
2023-09-04 22:06:21 +0200

    swagger token support

---
+++ src/WebAPI/Extensions/SwaggerConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.OpenApi.Models;
+using WebAPI.Attributes;
+
+namespace WebAPI.Extensions;
+
+public static class SwaggerConfiguration
+{
+    public static void ConfigureSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter your JWT token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+            c.SchemaFilter<DefaultValueSchemaFilter>();
+        });
+    }
+}
