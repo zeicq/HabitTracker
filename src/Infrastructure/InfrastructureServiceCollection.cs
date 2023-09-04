8d7af87a8bcf92a00973b2304c0037c5afd5a8e4
zeicq <patryk-zielinski@outlook.com>
2023-09-04 22:04:05 +0200

    Move Identity configuration to WebAPI.csproj

--- src/Infrastructure/InfrastructureServiceCollection.cs
+++ src/Infrastructure/InfrastructureServiceCollection.cs
@@ -2,7 +2,6 @@
 using Infrastructure.Persistence.Contexts;
 using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Identity;
-
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -15,31 +14,11 @@ public static class InfrastructureServiceCollection
     public static IServiceCollection InfrastructureServices(this IServiceCollection services,
         IConfiguration configuration)
     {
-        var identityOptions = configuration.GetSection("IdentityOptions");
-       
         services.AddDbContext<MssqlDbContext>(options =>
             options.UseSqlServer(
                 configuration.GetConnectionString("AzureConnection"),
                 b => b.MigrationsAssembly(typeof(MssqlDbContext).Assembly.FullName)));
         services.AddTransient(typeof(IGenericRepositoryBaseAsync<>), typeof(GenericRepositoryBaseAsync<>));
-
-        services.AddIdentity<IdentityUser, IdentityRole>(
-                options =>
-                {
-                    options.Password.RequireDigit = identityOptions.GetValue<bool>("Password:RequireDigit");
-                    options.Password.RequireLowercase = identityOptions.GetValue<bool>("Password:RequireLowercase");
-                    options.Password.RequireUppercase = identityOptions.GetValue<bool>("Password:RequireUppercase");
-                    options.Password.RequireNonAlphanumeric = identityOptions.GetValue<bool>("Password:RequireNonAlphanumeric");
-                    options.Password.RequiredLength = identityOptions.GetValue<int>("Password:RequiredLength");
-                    options.Password.RequiredUniqueChars = identityOptions.GetValue<int>("Password:RequiredUniqueChars");
-            
-                    options.User.RequireUniqueEmail = identityOptions.GetValue<bool>("User:RequireUniqueEmail");
-                    options.SignIn.RequireConfirmedEmail = identityOptions.GetValue<bool>("SignIn:RequireConfirmedEmail");
-                })
-            .AddEntityFrameworkStores<MssqlDbContext>()
-            .AddDefaultTokenProviders();
-
-
 
         services.AddTransient<IHabitRepository, HabitRepository>();
         services.AddTransient<IScheduleRepository, ScheduleRepository>();
---
+++ src/WebAPI/Extensions/IdentityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Persistence.Contexts;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAPI.Extensions;
+
+public static class IdentityConfigurationExtensions
+{
+    public static void ConfigureIdentityOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        var identityOptions = configuration.GetSection("IdentityOptions");
+        services.AddIdentity<IdentityUser, IdentityRole>(
+                options =>
+                {
+                    options.Password.RequireDigit = identityOptions.GetValue<bool>("Password:RequireDigit");
+                    options.Password.RequireLowercase = identityOptions.GetValue<bool>("Password:RequireLowercase");
+                    options.Password.RequireUppercase = identityOptions.GetValue<bool>("Password:RequireUppercase");
+                    options.Password.RequireNonAlphanumeric = identityOptions.GetValue<bool>("Password:RequireNonAlphanumeric");
+                    options.Password.RequiredLength = identityOptions.GetValue<int>("Password:RequiredLength");
+                    options.Password.RequiredUniqueChars = identityOptions.GetValue<int>("Password:RequiredUniqueChars");
+
+                    options.User.RequireUniqueEmail = identityOptions.GetValue<bool>("User:RequireUniqueEmail");
+                    options.SignIn.RequireConfirmedEmail = identityOptions.GetValue<bool>("SignIn:RequireConfirmedEmail");
+                })
+            .AddEntityFrameworkStores<MssqlDbContext>()
+            .AddDefaultTokenProviders();
+    }
+}
