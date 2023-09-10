8b95e1eccd6ddbfc032ead330af93240199bf8ed
zeicq <patryk-zielinski@outlook.com>
2023-09-10 16:04:22 +0200

    test upgrade

--- src/UnitTest/Features/CreateHabitCommandHandlerTests.cs
+++ src/UnitTest/Features/CreateHabitCommandHandlerTests.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using Domain.Entity;
 using Domain.Interfaces;
 using FluentAssertions;
+using Microsoft.Extensions.Caching.Memory;
 using Moq;
 
 namespace UnitTest.Features;
@@ -12,11 +13,12 @@ public class CreateHabitCommandHandlerTests
 {
     private readonly Mock<IHabitRepository> _habitRepositoryMock;
     private readonly Mock<IMapper> _mapperMock;
-
+    private readonly Mock<IMemoryCache> _memoryCache;
     public CreateHabitCommandHandlerTests()
     {
         _habitRepositoryMock = new Mock<IHabitRepository>();
         _mapperMock = new Mock<IMapper>();
+        _memoryCache = new Mock<IMemoryCache>();
     }
 
     [Fact]
@@ -24,14 +26,11 @@ public class CreateHabitCommandHandlerTests
     {
         // Arrange
         var command = new CreateHabitCommand();
-
-        _habitRepositoryMock.Setup(repo => repo.AddAsync(It.IsAny<Habit>()))
-            .ReturnsAsync(new Habit());
-
+        
         _mapperMock.Setup(mapper => mapper.Map<HabitViewModel>(It.IsAny<Habit>()))
             .Returns(new HabitViewModel());
 
-        var handler = new CreateHabitCommandHandler(_habitRepositoryMock.Object, _mapperMock.Object);
+        var handler = new CreateHabitCommandHandler(_habitRepositoryMock.Object, _mapperMock.Object,_memoryCache.Object);
 
         // Act
         var result = await handler.Handle(command, default);
