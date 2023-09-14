0e9971e312b4bd2b6987d6b2b2c56a16954bc47b
zeicq <patryk-zielinski@outlook.com>
2023-09-14 21:37:51 +0200

    Add IntegratedTest for Habit

---
+++ src/IntegrationTest/HabitControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace IntegrationTest;
+
+public class HabitControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public HabitControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task ShouldBeUnauthorized()
+    {
+        var client = _factory.CreateClient();
+        var response =await client.GetAsync($"Habit/{1}");
+        var status = response.StatusCode;
+        Assert.True((response.StatusCode == HttpStatusCode.Unauthorized));
+    }
+}
